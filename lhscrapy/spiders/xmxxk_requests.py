9d6470f908d20477006801d6711fe7983e711c44
lhsheild <lhsheild@yahoo.com>
2019-11-16 21:49:22 +0800

    selenium登陆爬取

--- lhscrapy/spiders/xmxxk_requests.py
+++ lhscrapy/spiders/xmxxk_requests.py
@@ -2,6 +2,7 @@ import json
 import re
 import time
 
+from scrapy.http import HtmlResponse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,13 +12,21 @@ from selenium.webdriver.support.ui import WebDriverWait
 def get_browser():
     chrome_driver = r"C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
     options = webdriver.ChromeOptions()
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_experimental_option('excludeSwitches', ['enable-automation'])
     browser = webdriver.Chrome(chrome_driver, options=options)
     return browser
 
 
 def login(username, password):
+    time1 = time.time()
+    """
+    selenium模拟登陆并爬取数据
+    :param username:
+    :param password:
+    :return:
+    """
+    # 登陆
     browser = get_browser()
     browser.get('http://202.103.199.210:8050/cas3/login')
     username_input = WebDriverWait(browser, 10).until(
@@ -32,8 +41,8 @@ def login(username, password):
         EC.element_to_be_clickable((By.ID, 'submit'))
     )
     submit_input.click()
-    print(browser.title)
 
+    # 保存cookie到文件
     browser.get('http://202.103.199.210:8070/nnxmgl/nnxmgl/xmtb/xmxx/TzxmProjectbaseinfoListPage.jspx?type=30')
     browser_cookies = browser.get_cookies()
     cookies_str_lst = []
@@ -44,13 +53,13 @@ def login(username, password):
     with open('cookie.txt', 'w') as f:
         f.write(json.dumps(browser_cookies))
 
+    # 获取所有项目guid
     all_project_id_lst = []
     while True:
         time.sleep(1)
         trs = WebDriverWait(browser,5).until(
             EC.presence_of_all_elements_located((By.XPATH, '//tbody[@id="templateform:reFreshData_data"]/tr'))
         )
-        # trs = browser.find_elements_by_xpath('//tbody[@id="templateform:reFreshData_data"]/tr')
         for tr in trs:
             guid_btn = tr.find_element_by_xpath('./td[4]/div/a')
             guid = guid_btn.get_attribute('onclick')
@@ -59,6 +68,7 @@ def login(username, password):
             new_temp = re_obj.group(1)
             new_re_str = r"'(.*)','"
             guid = re.match(new_re_str, new_temp).group(1)
+            print(guid)
             all_project_id_lst.append(guid)
         next_btn = browser.find_element_by_xpath('//div[@class="ui-paginator-wrap"]/span[4]')
         if not next_btn.get_attribute('class') == "ui-paginator-next ui-state-default ui-corner-all ui-state-disabled":
@@ -66,13 +76,23 @@ def login(username, password):
         else:
             break
 
+    # selenium请求项目详细信息并爬取
     for i in all_project_id_lst:
         edit_url = "http://202.103.199.210:8070/nnxmgl/nnxmgl/xmtb/xmxx/TzxmProjectEditAllinfo.jspx?guid={}".format(i)
         browser.get(edit_url)
+        # time.sleep(5)
+        # html = HtmlResponse(url=browser.current_url, body=browser.page_source, encoding='utf-8', status=200)
+        time.sleep(2)
+        browser.switch_to.frame(0)
+        project_no = browser.find_element_by_xpath('//span[@class="text-special"]').text
         project_name = browser.find_element_by_id('templateform:accordion1:projname').get_attribute('value')
-        print(project_name)
+        people = browser.find_element_by_xpath('//*[@id="templateform:addpanel"]/div[2]/div/div[2]/div/div[10]/div').text
+        # print(time.time()-time1)
         pass
 
+    # 关闭selenium
+    browser.quit()
+    print(time.time()-time1)
     return browser_cookies, cookies_str_lst, 'cookie.txt', all_project_id_lst
 
 
